335b77ea41253f724a08ae4f03d325254c94f46b
윤석규 <yskyu93@modutech.co.kr>
2022-11-04 17:17:01 +0900

    feat(engine): add sample process

--- module/engine.js
+++ module/engine.js
@@ -1,31 +1,73 @@
-function Runner (spec) {
-  let status = 'ready'
-  let process = null
+import { logger } from './logger.js'
+
+function makeProcess (spec) {
+  let timeout = null
+  let interval = null
+  function run (onData = (e) => {}) {
+    timeout = setTimeout(() => {
+      clearInterval(interval)
+      onData({
+        status: 'finished',
+        data: 'finish test'
+      })
+    }, Math.floor(Math.random() * 7000 + 3000))
+
+    interval = setInterval(() => {
+      if (Math.random() < 0.001) {
+        onData({
+          status: 'errored',
+          data: new Error('onError test')
+        })
+        clearInterval(interval)
+        clearTimeout(timeout)
+        return
+      }
+      onData({
+        status: 'running',
+        data: 'data test'
+      })
+    }, 100)
+  }
+  function stop () {
+    clearTimeout(timeout)
+    clearInterval(interval)
+  }
 
   return {
-    status,
-    run: () => {
-      if (status === 'running') throw new Error('already running')
-      status = 'running'
-      try {
-        process = () => {}
-        // [TODO]
-        status = 'finished'
-      } catch (e) {
-        status = 'errored'
-        throw e
-      }
-    },
-    stop: () => {
-      if (status !== 'running') return
-      status = 'ready'
-      process = null
+    run,
+    stop
+  }
+}
+
+function Process (spec) {
+  let status = 'ready'
+  const process = makeProcess(spec)
+
+  function run (onData = (e) => {}) {
+    if (status === 'running') {
+      onData({
+        status: 'errored',
+        data: new Error('already running')
+      })
+      return
     }
+    status = 'running'
+    process.run(onData)
+  }
+
+  function stop () {
+    status = 'ready'
+    process.stop()
+  }
+  return {
+    getStatus: () => status,
+    run,
+    stop
   }
 }
 
 function Piece (spec) {
-  const runner = Runner(spec)
+  const process = Process(spec)
   const sources = new Set()
   const targets = new Set()
 
@@ -39,18 +81,20 @@ function Piece (spec) {
   }
 
   return {
-    runner,
+    process,
     addSource,
     addTarget,
     isRunnable,
-    run: runner.run
+    run: process.run
   }
 }
 
 function LoadMap (cells, links) {
-  const cellIds = Object.keys(cells)
-  const map = new Map()
-  cellIds.forEach((cellId) => map.set(cellId, Piece()))
+  const { cellIds, map } = cells.reduce(({ cellIds, map }, { id, ...spec }) => {
+    cellIds.push(id)
+    map.set(id, Piece(spec))
+    return { cellIds, map }
+  }, { cellIds: [], map: new Map() })
 
   links.forEach(({ sourceId, targetId }) => {
     const source = map.get(sourceId)
@@ -59,21 +103,30 @@ function LoadMap (cells, links) {
     source.addTarget(target)
   })
 
-  const getRunnableIds = () => cellIds.reduce((bucket, cellId) => {
-    const piece = map.get(cellId)
+  const getRunnablePieces = () => cellIds.reduce((bucket, id) => {
+    const piece = map.get(id)
     const runnable = piece.isRunnable()
     runnable && bucket.push(piece)
     return bucket
   }, [])
   return {
     map,
-    getRunnableIds
+    getRunnablePieces
   }
 }
 
 export function Engine (spec) {
-  const runners = new Map()
   const cells = spec?.cells || {}
   const links = spec?.links || []
   const loadMap = LoadMap(cells, links)
+  const mic = { notify: logger.debug }
+
+  ;(function run (mic) {
+    loadMap.getRunnablePieces().forEach(id => {
+      loadMap.map.get(id).run(e => {
+        mic.notify(e)
+        if (e.type === 'finished') return run(mic)
+      })
+    })
+  })(mic)
 }
